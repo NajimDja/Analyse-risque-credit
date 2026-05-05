af2f7469e1c5c398d5713130f0daac9d6381d6b4
Najim Djareddir <najprgm@gmail.com>
2026-05-05 19:28:05 +0200

    Transformation, Analyse et quality des données

---
+++ src/data_analyse.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+
+class FonctionsStats:
+
+    def valeur_modale(self, series : pd.Series):
+        return series.mode()
+
+    def moyenne(self, series : pd.Series):
+        return series.mean().round(2)
+
+    def mediane(self, series : pd.Series):
+        return series.median().round(2)
+
+    def maximum(self, series : pd.Series):
+        return series.max().round(2)
+    
+    def minimum(self, series : pd.Series):
+        return series.min().round(2)
+    
+    def etendue(self, series : pd.Series):
+        return (series.max() - series.min()).round(2)
+    
+    def somme(self, series : pd.Series):
+        return series.sum().round(2)
+
+    def quantile(self, series : pd.Series, quant : float):
+        return series.quantile(q = quant).round(2)
+
+    def variance(self, series : pd.Series, ddof : int = 1):
+        return series.var(ddof=ddof).round(2)
+
+    def ecart_type(self, series : pd.Series, ddof : int = 1):
+        return series.std(ddof=ddof).round(2)
+    
+    def unique_vals(self, series : pd.Series):
+        print(f"Valeurs uniques de la colonne {series.name} :\n{series.unique()}")
+
+
+class DataAnalyse(FonctionsStats):
+
+    def analyse_var_numeriques(self, df : pd.DataFrame):
+        """Analyse des statistiques des variables numériques"""
+
+        df_stats = []
+        # cols_num = [j for j in df.columns if np.issubdtype(df[j].dtype, np.number)]
+        cols_num = [col for col in df.columns if pd.api.types.is_numeric_dtype(df[col])]   
+
+        print(f"Analyse des variables numériques :\n{', '.join(cols_num)}")
+        for i in cols_num:
+            df_stats.append({
+                'moyenne' : self.moyenne(df[i]),
+                'quantile 25%' : self.quantile(df[i], quant=0.25),
+                'mediane (50%)' : self.mediane(df[i]),
+                'quantile 75%' : self.quantile(df[i], quant=0.75),
+                'maximum' : self.maximum(df[i]),
+                'minimum' : self.minimum(df[i]),
+                'etendue' : self.etendue(df[i]),
+                'somme' : self.somme(df[i]),
+                'variance' : self.variance(df[i]),
+                'ecart type' : self.ecart_type(df[i])
+            })
+        df_stats = pd.DataFrame(df_stats)
+        df_stats.index = cols_num
+        return df_stats
+    
+    def analyse_var_categorielles(self, df : pd.DataFrame):
+        """Analyse des statistiques des variables catégorielles"""
+        
+        print("Analyse des variables catégorielles :")
+        for j in df.columns:
+            if np.issubdtype(df[j].dtype, np.number):
+                pass
+            else:
+                print(df.groupby(by = j)\
+                    .size()\
+                    .reset_index(name = 'nb')\
+                    .sort_values(by="nb", ascending=False)\
+                    .reset_index(drop=True))
+
+
+    def analyse_var_temporelles(self, df : pd.DataFrame):
+        """Analyse des variables temporelles"""
+        
+        print("Analyse des variables temporelles :")
+        col_date = [x for x in df.columns if pd.api.types.is_datetime64_any_dtype(df[x])]
+        
+        # Calcul de l'intervalle de temps
+        for c in col_date:
+            print("\nStatistiques de la variable temporelle : ", c)
+
+            print(df[c].describe())
+            intervalle = max(df[c]) - min(df[c])
+
+            # Conversion en années, mois et jours
+            annees = intervalle.days // 365
+            mois = (intervalle.days % 365) // 30
+            jours = (intervalle.days % 365) % 30
+
+            print(f"Intervalle de temps des données de la colonnes {c} : {annees} années, {mois} mois, et {jours} jours")
--- src/data_quality.py
+++ src/data_quality.py
@@ -1,6 +1,7 @@
 # Methodes d'analyse de la qualité de la données
 # Ecriture d'un rapport sur la qualité de la données
 import pandas as pd
+import re
 
 class DataQuality:
 
@@ -8,6 +9,7 @@ class DataQuality:
         pass
 
     def check_nulls(self, df : pd.DataFrame):
+        """Check le nombre, pourcentage de nulls par colonne"""
         df_null = []
         for i in df.columns:
             nb_null = df[i].isna().sum()
@@ -17,3 +19,54 @@ class DataQuality:
                             '% de null' : prct_null})
         df_null = pd.DataFrame(df_null)
         print(df_null)
+
+
+    def check_types(self, df : pd.DataFrame):
+        """Affiche le type de chaque colonne"""
+        print(df.dtypes)
+
+
+    def check_unique_values(self, df : pd.DataFrame, cols  : list[str] | None = None):
+        """Check si le dataframe a des lignes dupliquées"""
+        total_rows = len(df)
+        unique_rows = len(df.drop_duplicates(subset=cols))
+        print(f"Nombre de lignes totales : {total_rows}")
+        print(f"Nombre de lignes uniques : {unique_rows}")
+        print(f"Nombre de lignes dupliquées : {total_rows - unique_rows}")
+
+
+    def check_date_format(self, df: pd.DataFrame, threshold: float = 0.6):
+        """Check les colonnes avec des données au format de dates"""
+        
+        patterns = [
+            r"^\d{4}[-/]\d{2}[-/]\d{2}$",          # 2024-12-31, 2024/12/31
+            r"^\d{2}[-/]\d{2}[-/]\d{4}$",          # 31-12-2024, 31/12/2024
+            r"^\d{4}[-/]\d{2}[-/]\d{2}\s+\d{2}:\d{2}:\d{2}$",  # datetime complet
+            r"^\d{4}[-/]\d{2}[-/]\d{2}T\d{2}:\d{2}:\d{2}$",     # ISO 8601 simplifié
+        ]
+
+        date_cols = []
+        cols = []
+
+        for col in df.columns:
+            len_col = len(df[col])
+            s = df[col].dropna().astype(str).str.strip()
+            if s.empty:
+                continue
+
+            total = len(s)
+            matches = sum(
+                s.str.match(pat, na=False).sum()
+                for pat in patterns
+            )
+
+            if total > 0 and (matches / total) >= threshold:
+                date_cols.append({"Colonne" : col, 
+                                  "Cohérence sur lignes valides" : str(matches) + "/" + str(total), 
+                                  "Total lignes" : len_col})
+                cols.append(col)
+
+        date_cols = pd.DataFrame(date_cols)
+        print(f"Colonnes ayant des données au format de date\n")
+        print(date_cols)
+        return cols
